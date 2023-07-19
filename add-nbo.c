4b7d308b22fc521e9e25fd8448463188c585e38d
neko_hat <dohwan90@gmail.com>
2023-07-19 22:36:57 +0900

    Edit Function

--- add-nbo.c
+++ add-nbo.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <arpa/inet.h>
 
-uint32_t convert_endian(uint32_t target)
+/*uint32_t convert_endian(uint32_t target)
 {
     return ((target << 24) & 0xff000000 | (target << 8) & 0x00ff0000 \
     | (target >> 8) & 0x0000ff00| (target >> 24) & 0x000000ff);
-}
+}*/
 
 int main(int argc, char *argv[])
 {   
@@ -33,10 +34,10 @@ int main(int argc, char *argv[])
     uint32_t tmp2 = 0;
 
     fread(&tmp1, 4, 1, fp1);
-    tmp1 = convert_endian(tmp1);
+    tmp1 = ntohl(tmp1);
     
     fread(&tmp2, 4, 1, fp2);
-    tmp2 = convert_endian(tmp2);
+    tmp2 = ntohl(tmp2);
 
     uint32_t tmp3 = tmp1 + tmp2;
     printf("%d(%#x) + %d(%#x) = %d(%#x)\n", tmp1, tmp1, tmp2, tmp2, tmp3, tmp3);
